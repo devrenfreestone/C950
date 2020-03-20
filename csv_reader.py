9c7a4089cac0038272c7157a41424a9a57fa9653
Devren Freestone <devrenfreestone@gmail.com>
2020-03-19 20:09:47 -0700

    First commit

---
+++ csv_reader.py
@@ -0,0 +1,46 @@
+import csv
+
+with open('WGUPS_Package_File.csv') as package_file:
+    csv_reader = csv.reader(package_file, delimiter=',')
+    # for line in csv_reader:
+         # print(line)
+
+
+
+
+with open('WGUPS_Distance_Table.csv') as distance_file:
+    csv_reader = csv.reader(distance_file, delimiter=',')
+    distanceData = []
+    distance = 0
+    item = 0.1
+    delivered = []
+    next(csv_reader)
+    column = 0
+    j = 1
+    for row in csv_reader:
+        shortest = float(100.0)
+        item = float(0.1)
+        length = len(row)
+        i = 1
+
+        column = 0
+        # print(delivered)
+
+        while i < length:
+            if i in delivered:
+                pass
+            elif float(row[i]) != float(0):
+                item = float(row[i])
+                if item < shortest:
+                    shortest = item
+                    column = i
+            i += 1
+        print("row " + str(j) + ": " + " shortest: " + str(shortest) + " column: " + str(column))
+        delivered.append(column)
+        distance += shortest
+        j += 1
+        # print("shortest distance is package: " + str(column) + " which is: " + str(shortest) + " miles away.")
+    # for row in distanceData:
+    #     while i < row
+
+print(distance)
